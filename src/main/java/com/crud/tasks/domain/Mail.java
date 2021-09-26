0b85bfc507394a8fdc22753795c852088f20165a
Gabriela Miłoszewska <gabriela_miloszewska@wp.pl>
2021-09-26 09:45:28 +0200

    19.3. Wysyłanie maili z aplikacji

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -17,41 +17,4 @@ public class Mail {
     private final String subject;
     private final String message;
 
-    public static class MailBuilder {
-        private String mailTo;
-        private List<String> toCc = new ArrayList<>();
-        private String subject;
-        private String message;
-
-        public MailBuilder setMailTo(String mailTo) {
-            this.mailTo = mailTo;
-            return this;
-        }
-
-        public MailBuilder setToCc(String cc) {
-            toCc.add(cc);
-            return this;
-        }
-
-        public MailBuilder setSubject(String subject) {
-            this.subject = subject;
-            return this;
-        }
-
-        public MailBuilder setMessage(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public Mail build() {
-            return new Mail(mailTo, toCc, subject, message);
-        }
-    }
-
-    private Mail (final String mailTo, List<String> toCc, String subject, String message) {
-        this.mailTo = mailTo;
-        this.toCc = new ArrayList<>(toCc);
-        this.subject = subject;
-        this.message = message;
-    }
 }
